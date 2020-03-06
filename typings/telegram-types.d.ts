5a92471ce84b461175dff26f7210db056aa13e9a
frontendmonster <frontendmonster@gmail.com>
2020-03-06 08:26:04 +0330

    Add missing MemberStatus

--- typings/telegram-types.d.ts
+++ typings/telegram-types.d.ts
@@ -6,6 +6,129 @@ import * as fs from "fs";
 
 export type ParseMode = "Markdown" | "MarkdownV2" | "HTML";
 
+export type MemberStatus =
+  | "creator"
+  | "administrator"
+  | "member"
+  | "restricted"
+  | "left"
+  | "kicked";
+
+export type UserActions =
+  | "until_date"
+  | "can_be_edited"
+  | "can_change_info"
+  | "can_post_messages"
+  | "can_edit_messages"
+  | "can_delete_messages"
+  | "can_invite_users"
+  | "can_restrict_members"
+  | "can_pin_messages"
+  | "can_promote_members"
+  | "can_send_messages"
+  | "can_send_media_messages"
+  | "can_send_other_messages"
+  | "can_add_web_page_previews";
+
+export interface ChatMember {
+  /**
+   * Information about the user
+   */
+  user: TT.User;
+
+  /**
+   * The member's status in the chat. Can be “creator”, “administrator”,
+   * “member”, “restricted”, “left” or “kicked”
+   */
+  status: MemberStatus;
+
+  /**
+   * Restricted and kicked only. Date when restrictions will be lifted for
+   * this user, unix time
+   */
+  until_date?: number;
+
+  /**
+   * Administrators only. True, if the bot is allowed to edit administrator
+   * privileges of that user
+   */
+  can_be_edited?: boolean;
+
+  /**
+   * Administrators only. True, if the administrator can change the chat
+   * title, photo and other settings
+   */
+  can_change_info?: boolean;
+
+  /**
+   * Administrators only. True, if the administrator can post in the channel,
+   * channels only
+   */
+  can_post_messages?: boolean;
+
+  /**
+   * Administrators only. True, if the administrator can edit messages of
+   * other users and can pin messages, channels only
+   */
+  can_edit_messages?: boolean;
+
+  /**
+   * Administrators only. True, if the administrator can delete messages of
+   * other users
+   */
+  can_delete_messages?: boolean;
+
+  /**
+   * Administrators only. True, if the administrator can invite new users to
+   * the chat
+   */
+  can_invite_users?: boolean;
+
+  /**
+   * Administrators only. True, if the administrator can restrict, ban or
+   * unban chat members
+   */
+  can_restrict_members?: boolean;
+
+  /**
+   * Administrators only. True, if the administrator can pin messages,
+   * supergroups only
+   */
+  can_pin_messages?: boolean;
+
+  /**
+   * Administrators only. True, if the administrator can add new
+   * administrators with a subset of his own privileges or demote
+   * administrators that he has promoted, directly or indirectly (promoted by
+   * administrators that were appointed by the user)
+   */
+  can_promote_members?: boolean;
+
+  /**
+   * Restricted only. True, if the user can send text messages, contacts,
+   * locations and venues
+   */
+  can_send_messages?: boolean;
+
+  /**
+   * Restricted only. True, if the user can send audios, documents, photos,
+   * videos, video notes and voice notes, implies can_send_messages
+   */
+  can_send_media_messages?: boolean;
+
+  /**
+   * Restricted only. True, if the user can send animations, games, stickers
+   * and use inline bots, implies can_send_media_messages
+   */
+  can_send_other_messages?: boolean;
+
+  /**
+   * Restricted only. True, if user may add web page previews to his
+   * messages, implies can_send_media_messages
+   */
+  can_add_web_page_previews?: boolean;
+}
+
 export type ChatAction =
   | "typing"
   | "upload_photo"
